f56f097d4db9e6fbdde21ea798c0a466af08df82
João Victor de Faria Santana <24026811@fecap.br>
2026-09-24 22:00:14 -0300

    arrumando arquivos

--- check_word.ps1
+++
@@ -1,9 +0,0 @@
-try {
-    $word = [Runtime.InteropServices.Marshal]::GetActiveObject("Word.Application")
-    Write-Host "Word application found!"
-    foreach ($doc in $word.Documents) {
-        Write-Host "Doc Name: $($doc.Name) | FullName: $($doc.FullName)"
-    }
-} catch {
-    Write-Host "Error accessing Word COM: $($_.Exception.Message)"
-}
